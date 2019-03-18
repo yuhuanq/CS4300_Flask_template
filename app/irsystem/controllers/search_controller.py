19a5521440fd3c5f0375059d2f1e80b7ac26c845
Yuhuan Qiu <yq56@cornell.edu>
2019-03-18 18:13:09 -0500

    add project name, netids

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -1,9 +1,9 @@
-from . import *  
+from . import *
 from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
 
-project_name = "Ilan's Cool Project Template"
-net_id = "Ilan Filonenko: if56"
+project_name = "Song Finder"
+net_id = "James Zhou: jlz44, Tristan Stone: tjs264, Dylan Hecht: dkh55, Yiwen Huang: yw385, Yuhuan Qiu: yq56"
 
 @irsystem.route('/', methods=['GET'])
 def search():
