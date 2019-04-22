d98e7cc8a31b93d32c270526eb126a4be66072f2
Tristan Stone <tjs264@cornell.edu>
2019-04-22 18:49:13 -0400

    larger dataset, filter for release date

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -1,6 +1,7 @@
 from . import *
 from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
+from datetime import datetime
 import json
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -66,7 +67,26 @@ index_to_annotation = {i:v for i, v in enumerate(vectorizer.get_feature_names())
 index_to_id = {i:v for i, v in enumerate(list(annotation_to_text.keys()))}
 
 
-def find_most_similar(query,n_results):
+def should_filter(start, end, song_id):
+    if start:
+        start = datetime.strptime(start,"%Y-%m-%d")
+    if end:
+        end = datetime.strptime(end,"%Y-%m-%d")
+
+    release_date_str = all_songs[song_id]["release_date"]
+
+    if release_date_str:
+        release_date = datetime.strptime(release_date_str,"%Y-%m-%d")
+
+        #invalidate songs outside of date range
+        if (start and release_date<start) or (end and release_date>end):
+            return True
+    else:
+        return True
+
+    return False
+
+def find_most_similar(query,n_results, start = None, end = None):
     """
     finds n most similar annotations to query
     """
@@ -92,9 +112,9 @@ def find_most_similar(query,n_results):
     max_sim_sum = 0
     max_song_page_views = 0
     for annotation_id, sim_score in zip(annotation_ids, sim_scores):
-        if sim_score == 0:
-            continue
         song_id = annotation_to_song[annotation_id]
+        if sim_score == 0 or should_filter(start, end, song_id):
+            continue
         if song_id not in song_id_to_annotations:
             song_id_to_annotations[song_id] = []
         song_id_to_annotations[song_id].append((annotation_id, sim_score))
@@ -156,12 +176,13 @@ def find_most_similar(query,n_results):
 @irsystem.route('/', methods=['GET'])
 def search():
   query = request.args.get('search')
+  start = request.args.get('start')
   if not query:
     data = None
     output_message = ''
   else:
     output_message = "Your search: " + query
-    data = find_most_similar(query, 50)
+    data = find_most_similar(query, 50, start='2017-05-05')
   return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data)
 
 
