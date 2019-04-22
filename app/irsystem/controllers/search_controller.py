f04f367299e1512a9eac80565125376082bdee48
Yuhuan Qiu <yq56@cornell.edu>
2019-04-22 16:51:47 -0500

    group annotations together; linear comb with page views

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -6,6 +6,7 @@ import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel #same as cosine similarity
 from collections import Counter
+from functools import reduce
 
 project_name = "Song Finder"
 net_id = "James Zhou: jlz44, Tristan Stone: tjs264, Dylan Hecht: dkh55, Yiwen Huang: yw385, Yuhuan Qiu: yq56"
@@ -86,29 +87,71 @@ def find_most_similar(query,n_results):
     #find ids of most similar annotations
     annotation_ids = [index_to_id[index] for index in related_docs_indices] #can later be used to find lyric fragment maybe
 
+    # group them by songs
+    song_id_to_annotations = {}
+    max_sim_sum = 0
+    max_song_page_views = 0
+    for annotation_id, sim_score in zip(annotation_ids, sim_scores):
+        if sim_score == 0:
+            continue
+        song_id = annotation_to_song[annotation_id]
+        if song_id not in song_id_to_annotations:
+            song_id_to_annotations[song_id] = []
+        song_id_to_annotations[song_id].append((annotation_id, sim_score))
+        song_id_to_annotations[song_id].sort(key=lambda x: x[1], reverse=True)
+        max_sim_sum = max(
+            max_sim_sum,
+            reduce(
+                lambda acc, x: acc + x[1],
+                song_id_to_annotations[song_id],
+                0,
+            )
+        )
+        max_song_page_views = max(max_song_page_views,
+                                  all_songs[song_id]['page_views'])
 
-    #define output and input data
-    output_array = [] #annotations sorted from most similar to least
-    for i in range(1,n_results+1):
-        _id = annotation_ids[-i]
-        song_id = annotation_to_song[_id]
-        data = {} #data object contains the below fields
-        data["id"] = _id
-        data["song"] = all_songs[song_id]["title"]
-        data["annotation"] = annotation_to_text[_id]
-        data["lyric"] = annotation_to_fragment[_id]
-        data["artist"] = all_songs[song_id]["artists_names"]
-        data["image"] = all_songs[song_id]["header_image_url"]
+    print("max_song_page_views", max_song_page_views)
+    print("max_sim_sum", max_sim_sum)
+
+    result = []
+    for song_id in song_id_to_annotations:
+        song = {}
+        song['id'] = song_id
+        song["song"] = all_songs[song_id]["title"]
+        song["artist"] = all_songs[song_id]["artists_names"]
+        song["image"] = all_songs[song_id]["header_image_url"]
         if not all_songs[song_id]["album"] == None:
-            data["album"] = all_songs[song_id]["album"]["full_title"]
+            song["album"] = all_songs[song_id]["album"]["full_title"]
         else:
-            data["album"] = "No album found"
-        data["similarity"] = sim_scores[-i]
-        data['release_date'] = all_songs[song_id]['release_date']
-        if data['similarity'] :
-            output_array.append(data)
+            song["album"] = "No album found"
+        song['release_date'] = all_songs[song_id]['release_date']
 
-    return output_array
+
+        song["annotations"] = [
+            {'text':annotation_to_text[aid],
+             'similarity': score,
+             'lyric': annotation_to_fragment[aid]
+            }
+            for aid, score in song_id_to_annotations[song_id]
+        ]
+
+        # TODO take into page_views (need to normalize though before weighting)
+        song['page_views'] = max(all_songs[song_id]['page_views'], 0)
+
+        # score calculation
+        similarity_sum_normalized = reduce(
+            lambda acc, x: acc + x[1],
+            song_id_to_annotations[song_id],
+            0,
+        )/max_sim_sum
+        page_views_normalized = song['page_views'] / max_song_page_views
+
+        song['score'] = .8 * similarity_sum_normalized + .2 * page_views_normalized
+
+        result.append(song)
+
+    result.sort(key = lambda x : x['score'], reverse = True)
+    return result
 
 @irsystem.route('/', methods=['GET'])
 def search():
